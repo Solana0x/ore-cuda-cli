c2fcea183c68a578dc0ffc45ce142400596ae9d1
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-18 00:44:07 +0530

    Update solver.cu

--- cuda/equix/src/solver.cu
+++ cuda/equix/src/solver.cu
@@ -5,7 +5,7 @@
 #include "solver.h"
 #include "context.h"
 #include "solver_heap.h"
-#include <hashx_endian.h>
+#include <../../hashx/src/hashx_endian.h>
 #include <string.h>
 #include <stdbool.h>
 #include <assert.h>
