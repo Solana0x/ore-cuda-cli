c4615bff2fb3226bb37edae1e096dfc3b25fdd23
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-17 20:07:48 +0530

    Update blake2.cu

--- cuda/hashx/src/blake2.cu
+++ cuda/hashx/src/blake2.cu
@@ -15,6 +15,7 @@ static const uint64_t blake2b_IV[8] = {
 };
 
 // Predefined Sigma Constants
+//better 
 static const uint8_t blake2b_sigma[12][16] = {
     { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
     { 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 },
