37e95583c9915bb8358718e2e0e411ef19e54ddc
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-17 19:19:36 +0530

    Update equix.cu

--- cuda/equix/src/equix.cu
+++ cuda/equix/src/equix.cu
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include <equix.h>
-#include <hashx.h>
+#include <../include/equix.h>
+#include <../../hashx/include/hashx.h>
 #include "context.h"
 #include "solver.h"
 #include <hashx_endian.h>
