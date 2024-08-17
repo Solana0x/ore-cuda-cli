a990500d4ce30befcc4afcdc5a65a96d31cef090
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-17 20:56:50 +0530

    Update context.cu\

--- cuda/hashx/src/context.cu
+++ cuda/hashx/src/context.cu
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
-#include <../include/hashx.h>
+#include <hashx.h>
 #include "context.h"
 #include "compiler.h"
 #include "program.h"
@@ -9,56 +9,47 @@
 #define STRINGIZE(x) STRINGIZE_INNER(x)
 
 #ifndef HASHX_SALT
-#define HASHX_SALT "HashX v1"
+#define HASHX_SALT HashX v1
 #endif
 
-// Device constant for Blake2b parameters
+// The definition remains here
 __device__ const blake2b_param hashx_blake2_params = {
     64, 0, 1, 1, 0, 0, 0, 0, { 0 }, STRINGIZE(HASHX_SALT), { 0 }
 };
 
-// Allocate and initialize hashx context
 hashx_ctx* hashx_alloc(hashx_type type) {
     hashx_ctx* ctx;
-
-    // Allocate unified memory for context
-    if (cudaMallocManaged(&ctx, sizeof(hashx_ctx)) != cudaSuccess) {
-        return NULL;
-    }
-
+    cudaMallocManaged(&ctx, sizeof(hashx_ctx));
+    
     ctx->code = NULL;
-    ctx->program = NULL;
-
-    // Initialize the context based on type
     if (type & HASHX_COMPILED) {
         if (!hashx_compiler_init(ctx)) {
             cudaFree(ctx);
             return NULL;
         }
         ctx->type = HASHX_COMPILED;
-    } else {
-        if (cudaMallocManaged(&ctx->program, sizeof(hashx_program)) != cudaSuccess) {
-            cudaFree(ctx);
-            return NULL;
-        }
+    }
+    else {
+        cudaMallocManaged(&ctx->program, sizeof(hashx_program));
         ctx->type = HASHX_INTERPRETED;
     }
-
+    
 #ifdef HASHX_BLOCK_MODE
-    // Copy the Blake2b parameters to the context
-    memcpy(&ctx->params, &hashx_blake2_params, sizeof(blake2b_param));
+    memcpy(&ctx->params, &hashx_blake2_params, 32);
 #endif
 
     return ctx;
 }
 
-// Free hashx context
 void hashx_free(hashx_ctx* ctx) {
     if (ctx != NULL && ctx != HASHX_NOTSUPP) {
-        if (ctx->type & HASHX_COMPILED) {
-            hashx_compiler_destroy(ctx);
-        } else if (ctx->program != NULL) {
-            cudaFree(ctx->program);
+        if (ctx->code != NULL) {
+            if (ctx->type & HASHX_COMPILED) {
+                hashx_compiler_destroy(ctx);
+            }
+            else {
+                cudaFree(ctx->program);
+            }
         }
         cudaFree(ctx);
     }
