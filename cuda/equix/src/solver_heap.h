6b2ab77799d33dab0c84fc539eb6b5e31e6c06e0
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-18 00:39:43 +0530

    Update solver_heap.h

--- cuda/equix/src/solver_heap.h
+++ cuda/equix/src/solver_heap.h
@@ -13,13 +13,16 @@
 #define COARSE_BUCKET_ITEMS 336
 #define FINE_BUCKET_ITEMS 12
 
+/* Use aligned memory for better cache performance */
+#define CACHE_LINE_SIZE 64
+
 typedef uint16_t fine_item;
 
-typedef struct fine_bucket {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) fine_bucket {
     fine_item items[FINE_BUCKET_ITEMS];
 } fine_bucket;
 
-typedef struct fine_hashtab {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) fine_hashtab {
     uint8_t counts[NUM_FINE_BUCKETS];
     fine_bucket buckets[NUM_FINE_BUCKETS];
 } fine_hashtab;
@@ -27,20 +30,20 @@ typedef struct fine_hashtab {
 typedef equix_idx stage1_idx_item;  /* 16 bits */
 typedef uint64_t stage1_data_item;  /* 52 bits */
 
-typedef struct stage1_idx_bucket {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage1_idx_bucket {
     stage1_idx_item items[COARSE_BUCKET_ITEMS];
 } stage1_idx_bucket;
 
-typedef struct stage1_data_bucket {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage1_data_bucket {
     stage1_data_item items[COARSE_BUCKET_ITEMS];
 } stage1_data_bucket;
 
-typedef struct stage1_idx_hashtab {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage1_idx_hashtab {
     uint16_t counts[NUM_COARSE_BUCKETS];
     stage1_idx_bucket buckets[NUM_COARSE_BUCKETS];
 } stage1_idx_hashtab;
 
-typedef struct stage1_data_hashtab {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage1_data_hashtab {
     stage1_data_bucket buckets[NUM_COARSE_BUCKETS];
 } stage1_data_hashtab;
 
@@ -48,11 +51,11 @@ typedef uint32_t stage2_idx_item; /* 26 bits: 8 bits = left bucket index
                                               9 bits = left item index
                                               9 bits = right item index */
 
-typedef struct stage2_idx_bucket {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage2_idx_bucket {
     stage2_idx_item items[COARSE_BUCKET_ITEMS];
 } stage2_idx_bucket;
 
-typedef struct stage2_idx_hashtab {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage2_idx_hashtab {
     uint16_t counts[NUM_COARSE_BUCKETS];
     stage2_idx_bucket buckets[NUM_COARSE_BUCKETS];
 } stage2_idx_hashtab;
@@ -69,28 +72,28 @@ typedef struct stage2_data_item {
 typedef uint64_t stage2_data_item;  /* 37 bits */
 #endif
 
-typedef struct stage2_data_bucket {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage2_data_bucket {
     stage2_data_item items[COARSE_BUCKET_ITEMS];
 } stage2_data_bucket;
 
-typedef struct stage2_data_hashtab {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage2_data_hashtab {
     stage2_data_bucket buckets[NUM_COARSE_BUCKETS];
 } stage2_data_hashtab;
 
 typedef uint32_t stage3_data_item;  /* 22 bits */
 
-typedef struct stage3_data_bucket {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage3_data_bucket {
     stage3_data_item items[COARSE_BUCKET_ITEMS];
 } stage3_data_bucket;
 
-typedef struct stage3_data_hashtab {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) stage3_data_hashtab {
     stage3_data_bucket buckets[NUM_COARSE_BUCKETS];
 } stage3_data_hashtab;
 
 typedef stage2_idx_hashtab stage3_idx_hashtab;
 typedef stage2_idx_item stage3_idx_item;
 
-typedef struct solver_heap {
+typedef struct __attribute__((aligned(CACHE_LINE_SIZE))) solver_heap {
     stage1_idx_hashtab stage1_indices;           /* 172,544 bytes */
     stage2_idx_hashtab stage2_indices;           /* 344,576 bytes */
     stage2_data_hashtab stage2_data;             /* 688,128 bytes */
