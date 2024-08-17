038c73a6e9d27016e9fcaf73f17ad95757d7216e
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-18 00:00:44 +0530

    Update siphash.h

--- cuda/hashx/src/siphash.h
+++ cuda/hashx/src/siphash.h
@@ -1,12 +1,13 @@
+/* Copyright (c) 2020 tevador <tevador@gmail.com> */
+/* See LICENSE for licensing information */
+
 #ifndef SIPHASH_H
 #define SIPHASH_H
 
 #include <stdint.h>
+#include <hashx.h>
 
-// Rotate left macro
 #define ROTL(x, b) (((x) << (b)) | ((x) >> (64 - (b))))
-
-// SipHash round function
 #define SIPROUND(v0, v1, v2, v3) \
   do { \
     v0 += v1; v2 += v3; v1 = ROTL(v1, 13);   \
@@ -16,63 +17,19 @@
     v1 ^= v2; v3 ^= v0; v2 = ROTL(v2, 32);   \
   } while (0)
 
-// SipHash state structure
 typedef struct siphash_state {
     uint64_t v0, v1, v2, v3;
 } siphash_state;
 
-// Function to compute SipHash-1-3
-uint64_t hashx_siphash13_ctr(uint64_t input, const siphash_state* keys) {
-    uint64_t v0 = keys->v0;
-    uint64_t v1 = keys->v1;
-    uint64_t v2 = keys->v2;
-    uint64_t v3 = keys->v3;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-    v3 ^= input;
+HASHX_PRIVATE uint64_t hashx_siphash13_ctr(uint64_t input, const siphash_state* keys);
+__device__ HASHX_PRIVATE void hashx_siphash24_ctr_state512(const siphash_state* keys, uint64_t input, uint64_t state_out[8]);
 
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-
-    v0 ^= input;
-    v2 ^= 0xff;
-
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-
-    return (v0 ^ v1) ^ (v2 ^ v3);
+#ifdef __cplusplus
 }
+#endif
 
-// Function to compute SipHash-2-4 and store state in an array
-void hashx_siphash24_ctr_state512(const siphash_state* keys, uint64_t input, uint64_t state_out[8]) {
-    uint64_t v0 = keys->v0;
-    uint64_t v1 = keys->v1;
-    uint64_t v2 = keys->v2;
-    uint64_t v3 = keys->v3;
-
-    v3 ^= input;
-
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-
-    v0 ^= input;
-    v2 ^= 0xff;
-
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-
-    // Store the result in the output array
-    state_out[0] = v0;
-    state_out[1] = v1;
-    state_out[2] = v2;
-    state_out[3] = v3;
-    state_out[4] = v0 ^ v1;
-    state_out[5] = v2 ^ v3;
-    state_out[6] = v0 ^ v2;
-    state_out[7] = v1 ^ v3;
-}
-
-#endif // SIPHASH_H
+#endif
