2a72443eb6fb8da00162da6c7bf0caff95b3b7b0
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-20 02:11:27 +0530

    Update drillx.cu

--- cuda/drillx.cu
+++ cuda/drillx.cu
@@ -8,7 +8,7 @@
 #include "equix/src/solver_heap.h"
 #include "hashx/src/context.h"
 
-const int BATCH_SIZE = 16384; 
+const int BATCH_SIZE = 10000; 
 const int NUM_HASHING_ROUNDS = 1; 
 
 #define CUDA_CHECK(call) \
