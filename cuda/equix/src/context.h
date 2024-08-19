50c2a8f0b6f6f6bc1dc6ae0ff5b00cbd07321172
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-20 02:14:57 +0530

    Update context.h

--- cuda/equix/src/context.h
+++ cuda/equix/src/context.h
@@ -5,10 +5,10 @@
 #include <../../hashx/include/hashx.h>
 
 typedef struct solver_heap solver_heap;
-typedef struct __attribute__((aligned(64))) equix_ctx {
-    hashx_ctx* __restrict__ hash_func;  // Use __restrict__ to help with potential pointer aliasing
-    solver_heap* __restrict__ heap;
-    equix_ctx_flags flags;
+typedef struct equix_ctx {
+	hashx_ctx* hash_func;
+	solver_heap* heap;
+	equix_ctx_flags flags;
 } equix_ctx;
 
 #endif
