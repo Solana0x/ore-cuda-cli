de8381609d96d74f99de249ddf3e8da84f629a5f
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-20 02:07:52 +0530

    Update solver.h

--- cuda/equix/src/solver.h
+++ cuda/equix/src/solver.h
@@ -7,23 +7,41 @@
 #include <../include/equix.h>
 #include <../../hashx/src/hashx_endian.h>
 #include <stdbool.h>
+#include <immintrin.h>  // Header for SIMD intrinsics
 #include "context.h"
 
 #define EQUIX_STAGE1_MASK ((1ull << 15) - 1)
 #define EQUIX_STAGE2_MASK ((1ull << 30) - 1)
 #define EQUIX_FULL_MASK ((1ull << 60) - 1)
 
-
 __device__ inline bool tree_cmp1(const equix_idx* left, const equix_idx* right) {
-	return *left <= *right;
+    // Load 16-bit values from both pointers
+    __m128i left_vec = _mm_loadu_si128((__m128i*)left);
+    __m128i right_vec = _mm_loadu_si128((__m128i*)right);
+    // Compare using SIMD
+    __m128i cmp_result = _mm_cmpeq_epi16(left_vec, right_vec);
+    // Return if left <= right for all elements
+    return _mm_movemask_epi8(cmp_result) != 0;
 }
 
 __device__ inline bool tree_cmp2(const equix_idx* left, const equix_idx* right) {
-	return load32(left) <= load32(right);
+    // Load 32-bit values from both pointers
+    __m128i left_vec = _mm_loadu_si128((__m128i*)left);
+    __m128i right_vec = _mm_loadu_si128((__m128i*)right);
+    // Compare using SIMD
+    __m128i cmp_result = _mm_cmpeq_epi32(left_vec, right_vec);
+    // Return if left <= right for all elements
+    return _mm_movemask_epi8(cmp_result) != 0;
 }
 
 __device__ inline bool tree_cmp4(const equix_idx* left, const equix_idx* right) {
-	return load64(left) <= load64(right);
+    // Load 64-bit values from both pointers
+    __m128i left_vec = _mm_loadu_si128((__m128i*)left);
+    __m128i right_vec = _mm_loadu_si128((__m128i*)right);
+    // Compare using SIMD
+    __m128i cmp_result = _mm_cmpeq_epi64(left_vec, right_vec);
+    // Return if left <= right for all elements
+    return _mm_movemask_epi8(cmp_result) != 0;
 }
 
 __device__ void hash_stage0i(hashx_ctx* hash_func, uint64_t* out, uint32_t i);
