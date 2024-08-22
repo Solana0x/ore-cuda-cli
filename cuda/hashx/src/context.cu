d5504df12db0352f2636043f7bde2a98bb4d358c
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-22 23:54:04 +0530

    Update context.cu

--- cuda/hashx/src/context.cu
+++ cuda/hashx/src/context.cu
@@ -18,37 +18,24 @@ __device__ const blake2b_param hashx_blake2_params = {
 };
 
 hashx_ctx* hashx_alloc(hashx_type type) {
-    hashx_ctx* ctx = NULL;
-
-    // Allocate unified memory for context
-    cudaError_t err = cudaMallocManaged(&ctx, sizeof(hashx_ctx));
-    if (err != cudaSuccess) {
-        return NULL;
-    }
-
-    // Initialize pointers to NULL
+    hashx_ctx* ctx;
+    cudaMallocManaged(&ctx, sizeof(hashx_ctx));
+    
     ctx->code = NULL;
-    ctx->program = NULL;
-
-    // Allocate memory based on the type of context
     if (type & HASHX_COMPILED) {
         if (!hashx_compiler_init(ctx)) {
             cudaFree(ctx);
             return NULL;
         }
         ctx->type = HASHX_COMPILED;
-    } else {
-        err = cudaMallocManaged(&ctx->program, sizeof(hashx_program));
-        if (err != cudaSuccess) {
-            cudaFree(ctx);
-            return NULL;
-        }
+    }
+    else {
+        cudaMallocManaged(&ctx->program, sizeof(hashx_program));
         ctx->type = HASHX_INTERPRETED;
     }
-
+    
 #ifdef HASHX_BLOCK_MODE
-    // Directly initialize the blake2b_param structure
-    ctx->params = hashx_blake2_params;
+    memcpy(&ctx->params, &hashx_blake2_params, 32);
 #endif
 
     return ctx;
@@ -56,10 +43,13 @@ hashx_ctx* hashx_alloc(hashx_type type) {
 
 void hashx_free(hashx_ctx* ctx) {
     if (ctx != NULL && ctx != HASHX_NOTSUPP) {
-        if (ctx->type & HASHX_COMPILED) {
-            hashx_compiler_destroy(ctx);
-        } else {
-            cudaFree(ctx->program);
+        if (ctx->code != NULL) {
+            if (ctx->type & HASHX_COMPILED) {
+                hashx_compiler_destroy(ctx);
+            }
+            else {
+                cudaFree(ctx->program);
+            }
         }
         cudaFree(ctx);
     }
