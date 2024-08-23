79d8089b0cb046f28e952a8653695de875781db0
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-24 02:30:33 +0530

    Update drillx.cu

--- cuda/drillx.cu
+++ cuda/drillx.cu
@@ -40,7 +40,7 @@ extern "C" void hash(uint8_t *challenge, uint8_t *nonce, uint64_t *out) {
         }
     }
 
-    int threadsPerBlock = 256;  // Increased number of threads per block
+    int threadsPerBlock = 64;  // Increased number of threads per block
     int blocksPerGrid = (BATCH_SIZE * INDEX_SPACE + threadsPerBlock - 1) / threadsPerBlock;
 
     cudaStream_t stream;
@@ -89,7 +89,7 @@ extern "C" void solve_all_stages(uint64_t *hashes, uint8_t *out, uint32_t *sols,
 
     CUDA_CHECK(cudaMemcpy(d_hashes, hashes, num_sets * INDEX_SPACE * sizeof(uint64_t), cudaMemcpyHostToDevice));
 
-    int threadsPerBlock = 256;
+    int threadsPerBlock = 64;
     int blocksPerGrid = (num_sets + threadsPerBlock - 1) / threadsPerBlock;
 
     solve_all_stages_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_hashes, d_heaps, d_solutions, d_num_sols);
