7b32f7eaca7799d15dafb6a549b97f81570d1263
0xSolana <142747768+Solana0x@users.noreply.github.com>
2024-08-24 02:44:45 +0530

    Update drillx.cu

--- cuda/drillx.cu
+++ cuda/drillx.cu
@@ -10,7 +10,7 @@
 #include "hashx/src/context.h"
 
 const int BATCH_SIZE = 8192;
-__constant__ int NUM_HASHING_ROUNDS = 1;  // Use __constant__ memory for NUM_HASHING_ROUNDS
+__constant__ int NUM_HASHING_ROUNDS = 0;  // Use __constant__ memory for NUM_HASHING_ROUNDS
 
 #define CUDA_CHECK(call) \
     do { \
